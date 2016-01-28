1d6e3cbb5669320cba060ff04971b4eb3b7b9eae
Friðberg Smárason <fridbergsmar@mail.com>
2016-01-29 06:33:02 +0900

    Create pyzzpuzz.py

---
+++ pyzzpuzz.py
@@ -0,0 +1,10 @@
+# Like a general
+
+def pyzzpuzz(n): 
+    for i in range(1, n+1): 
+        if i % 15 == 0: yield 'FizzBuzz' 
+        elif i % 5 == 0: yield 'Buzz' 
+        elif i % 3 == 0: yield 'Fizz' 
+        else: yield i
+
+for x in fizzbuzz(100): print x
